9c3f9990c5aab70b2a1e0f5e6f496be702a0238a
jhojoo <jaysys@gmail.com>
2023-10-08 13:50:09 +0000

    new file:   .gitignore new file:   get_llama2.sh renamed:    alpha/charlie.py -> mywork/algowork/charlie.py renamed:    alpha/echo.py -> mywork/algowork/echo.py renamed:    googleaccess.py -> mywork/googleaccess.py renamed:    plotwithstreamlit.py -> mywork/plotwithstreamlit.py new file:   mywork/tensorwork/alpha.py

---
+++ mywork/tensorwork/alpha.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+print("----------------------------------")
+print("tf", tf.__version__, "is installed")
+print("Thanks")
